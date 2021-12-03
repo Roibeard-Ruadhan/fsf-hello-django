96944e52028466fe320021ebe5430c15e1995099
Roibéard R Coughlan <roibeard.c@outlook.com>
2021-12-03 11:16:55 +0000

    Add list of tests for test_views.py to be completed

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,2 +1,16 @@
 from django.test import TestCase
-from .forms import ItemForm
+
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        
+    def test_get_add_item_page(self):
+
+    def test_get_edit_item_page(self):
+
+    def test_can_add_item(self):
+
+    def test_can_delete_item(self):
+
+    def test_can_toggle_item(self):
