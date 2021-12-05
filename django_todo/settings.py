2a12af77cf41d9ec4ce6d4035cbbd28688491278
Roibéard R Coughlan <roibeard.c@outlook.com>
2021-12-05 18:16:07 +0000

    Setup development environment, add if statement to Allowed_hosts on settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,8 +27,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-2n=lj1bnaekk25+$6^u+s
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
-
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
